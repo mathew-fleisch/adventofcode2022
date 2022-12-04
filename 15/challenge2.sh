e9e5b6fedcea1c33a981b079bd46ac35dabd8083
Mathew Fleisch <mathew.fleisch@AIDBELOWTARGET.workdayinternal.com>
2022-12-03 16:44:19 -0800

    Advent of Code 2022: Seed template

--- 15/challenge2.sh
+++ 15/challenge2.sh
@@ -1,2 +1,37 @@
 #!/bin/bash
-echo "Not Implemented"
+#shellcheck disable=SC2086
+convertsecs() {
+  ((h=${1}/3600))
+  ((m=(${1}%3600)/60))
+  ((s=${1}%60))
+  printf "%02d:%02d:%02d\n" $h $m $s
+}
+
+DEBUG=${DEBUG:-0}
+LOGFILE=${LOGFILE:-log-day15-challenge2.txt}
+inputFile=${1:-input.txt}
+started=$(date +%s)
+echo "started: $started" > $LOGFILE
+
+IFS=$'\n' read -d '' -r -a values < $inputFile;
+
+inputLength=${#values[@]}
+[ $DEBUG -eq 1 ] && echo "Lines: $inputLength"
+echo "Lines: $inputLength" >> $LOGFILE
+
+if ! declare -A map 2> /dev/null ; then
+  echo "Associative arrays not supported with this version of bash! Upgrade bash, or use the container provided."
+  /bin/bash --version | head -1
+  exit
+fi
+
+for line in "${values[@]}"; do
+    [ $DEBUG -eq 1 ] && echo "$line"
+done
+echo >> $LOGFILE
+echo "Not Implemented" | tee -a $LOGFILE
+
+now=$(date +%s)
+diff=$((now-started))
+echo "ended: $now" >> $LOGFILE
+echo "Completed in: $(convertsecs $diff)" >> $LOGFILE
