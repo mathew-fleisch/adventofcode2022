425fc9a7e60c35a6d8e289ea1465b42dd6397cae
Mathew Fleisch <mathew.fleisch@AIDBELOWTARGET.workdayinternal.com>
2022-12-04 13:58:27 -0800

    Day-04: Camp Cleanup

--- 04/challenge1.sh
+++ 04/challenge1.sh
@@ -25,11 +25,25 @@ if ! declare -A map 2> /dev/null ; then
   exit
 fi
 
+total=0
 for line in "${values[@]}"; do
-    [ $DEBUG -eq 1 ] && echo "$line"
+  [ $DEBUG -eq 1 ] && echo "$line"
+  elf1=$(echo "$line" | awk -F',' '{print $1}')
+  elf1low=$(echo "$elf1" | awk -F'-' '{print $1}')
+  elf1high=$(echo "$elf1" | awk -F'-' '{print $2}')
+  elf2=$(echo "$line" | awk -F',' '{print $2}')
+  elf2low=$(echo "$elf2" | awk -F'-' '{print $1}')
+  elf2high=$(echo "$elf2" | awk -F'-' '{print $2}')
+  [ $DEBUG -eq 1 ] && echo "Elf1: $elf1 -> $elf1low - $elf1high"
+  [ $DEBUG -eq 1 ] && echo "Elf2: $elf2 -> $elf2low - $elf2high"
+  if [[ $elf1low -ge $elf2low ]] && [[ $elf1high -le $elf2high ]]; then
+    total=$((total+1))
+  elif [[ $elf2low -ge $elf1low ]] && [[ $elf2high -le $elf1high ]]; then
+    total=$((total+1))
+  fi
 done
 echo >> $LOGFILE
-echo "Not Implemented" | tee -a $LOGFILE
+echo "$total" | tee -a $LOGFILE
 
 now=$(date +%s)
 diff=$((now-started))
--- 04/challenge2.sh
+++ 04/challenge2.sh
@@ -8,7 +8,7 @@ convertsecs() {
 }
 
 DEBUG=${DEBUG:-0}
-LOGFILE=${LOGFILE:-log-day04-challenge2.txt}
+LOGFILE=${LOGFILE:-log-day04-challenge1.txt}
 inputFile=${1:-input.txt}
 started=$(date +%s)
 echo "started: $started" > $LOGFILE
@@ -25,11 +25,27 @@ if ! declare -A map 2> /dev/null ; then
   exit
 fi
 
+total=0
 for line in "${values[@]}"; do
-    [ $DEBUG -eq 1 ] && echo "$line"
+  [ $DEBUG -eq 1 ] && echo "$line"
+  elf1=$(echo "$line" | awk -F',' '{print $1}')
+  elf1low=$(echo "$elf1" | awk -F'-' '{print $1}')
+  elf1high=$(echo "$elf1" | awk -F'-' '{print $2}')
+  elf2=$(echo "$line" | awk -F',' '{print $2}')
+  elf2low=$(echo "$elf2" | awk -F'-' '{print $1}')
+  elf2high=$(echo "$elf2" | awk -F'-' '{print $2}')
+  [ $DEBUG -eq 1 ] && echo "Elf1: $elf1 -> $elf1low - $elf1high"
+  [ $DEBUG -eq 1 ] && echo "Elf2: $elf2 -> $elf2low - $elf2high"
+  if [[ $elf1low -ge $elf2low ]] && [[ $elf1low -le $elf2high ]]; then
+    total=$((total+1))
+  elif [[ $elf2low -ge $elf1low ]] && [[ $elf2low -le $elf1high ]]; then
+    total=$((total+1))
+  elif [[ $elf1high -eq $elf2high ]] || [[ $elf1low -eq $elf2low ]] || [[ $elf1high -eq $elf2low ]] || [[ $elf1low -eq $elf2low ]]; then
+    total=$((total+1))
+  fi
 done
 echo >> $LOGFILE
-echo "Not Implemented" | tee -a $LOGFILE
+echo "$total" | tee -a $LOGFILE
 
 now=$(date +%s)
 diff=$((now-started))
